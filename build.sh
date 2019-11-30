65e40cd7d0a96cb3e0a9d591a91952feeceb3120
Nicol <nicolchawkins@gmail.com>
2019-11-30 00:40:07 -0800

    First Commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+cat templates/top.html content/blog.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
+
+
